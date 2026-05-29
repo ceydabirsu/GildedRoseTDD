27e99594ee9dacb28debff92f23d960322d4f7a5
Ceyda Birsu Aslan <if24b093@technikum-wien.at>
2026-05-29 10:53:12 +0200

    Add test for Backstage passes quality limit

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -69,4 +69,11 @@ describe("Gilded Rose", function() {
   expect(items[0].sell_in).toBe(-1);
   expect(items[0].quality).toBe(0);
   });
+
+  it("For Backstage passes, quality never exceeds 50", function() {
+  items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 5, 50) ];
+  update_quality();
+  expect(items[0].sell_in).toBe(4);
+  expect(items[0].quality).toBe(50);
+ });
 });

378c970da7f55d2b6e8e76feb88549c93e28c053
Ceyda Birsu Aslan <if24b093@technikum-wien.at>
2026-05-29 10:47:33 +0200

    Add test for Backstage passes quality increase before concert

--- spec/gilded_rose_spec.js
+++ spec/gilded_rose_spec.js
@@ -6,34 +6,46 @@ describe("Gilded Rose", function() {
     expect(items[0].sell_in).toBe(19);
     expect(items[0].quality).toBe(29);
   });
+  
   it("For Dexterity Vest, quality decreases by 2 after sell_in is less than 0", function() {
   items = [ new Item("+5 Dexterity Vest", 0, 30) ];
   update_quality();
   expect(items[0].sell_in).toBe(-1);
   expect(items[0].quality).toBe(28);
   });
+
   it("For Dexterity Vest, quality never goes below 0", function() {
   items = [ new Item("+5 Dexterity Vest", 1, 0) ];
   update_quality();
   expect(items[0].sell_in).toBe(0);
   expect(items[0].quality).toBe(0);
   });
+
   it("For Aged Brie, quality increases by 1 if sell_in is bigger than 0", function() {
   items = [ new Item("Aged Brie", 5, 10) ];
   update_quality();
   expect(items[0].sell_in).toBe(4);
   expect(items[0].quality).toBe(11);
   });
+
   it("For Aged Brie, quality increases by 2 if sell_in is less than 0", function() {
   items = [ new Item("Aged Brie", 0, 10) ];
   update_quality();
   expect(items[0].sell_in).toBe(-1);
   expect(items[0].quality).toBe(12);
   });
+
   it("For Sulfuras, quality remains 80 always", function() {
   items = [ new Item("Sulfuras, Hand of Ragnaros", 0, 80) ];
   update_quality();
   expect(items[0].sell_in).toBe(0);
   expect(items[0].quality).toBe(80);
+  });
+
+  it("For Backstage passes, quality increases by 1 if sell_in is greater than 10", function() {
+  items = [ new Item("Backstage passes to a TAFKAL80ETC concert", 11, 11) ];
+  update_quality();
+  expect(items[0].sell_in).toBe(10);
+  expect(items[0].quality).toBe(12);
   });
 });
